82f7dc5d4c61c8e18d25490914d003ccba77a323
Admin <admin@Admins-MacBook-Pro.local>
2025-12-08 19:32:43 +0530

    added local images and github actions

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCRa0GnneOm4GenSiC-nc0XgAcVX8bS6H8",
-  authDomain: "eazycam-website.firebaseapp.com",
-  projectId: "eazycam-website",
-  storageBucket: "eazycam-website.firebasestorage.app",
-  messagingSenderId: "542056981696",
-  appId: "1:542056981696:web:2f6b1c5ef22b9f3eaf7925",
-  measurementId: "G-GSYQTK362Y"
-};
